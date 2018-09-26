1cc81bfdae7477ad39050cc3ddf12715ee4d19ff
Idarlene Araujo <37625274+idarlenearaujo@users.noreply.github.com>
2018-09-26 16:06:21 -0300

    ~

--- PUCMG/Trabalho Adicional/trab_01.py
+++ PUCMG/Trabalho Adicional/trab_01.py
@@ -1,10 +1,6 @@
 import glob
 import mincemeat
 import csv
-import nltk
-nltk.download()
-from nltk.corpus import stopwords
-sw = stopwords.words("english")
 
 # arquivos
 files = glob.glob('Trab2.3\\*')
@@ -25,18 +21,25 @@ source = dict((file_name, ler_arquivos(file_name)) for file_name in files)
 # Name node especifica workers para executar a função MAP
 # criando nova chave e valor ordenados para serem processados pela função reduce
 def mapfn(k, v):
+
+    from nltk.corpus import stopwords
+    sw = stopwords.words("english")
+
     for line in v.splitlines():
         autores = line.split(':::')[1]
         title = line.split(':::')[2]
 
         # por autor
         for autor in autores.split('::'):
-            # frase
-            for phrase in title.splitlines():
-                # palavra
-                for word in phrase.split():
-                    if word not in sw:
-                        yield autor + ' :: ' + word, 1
+
+            if (autor == 'Grzegorz Rozenberg') or (autor == 'Philip S. Yu'):
+                # frase
+                for phrase in title.splitlines():
+                    # palavra
+                    for word in phrase.split():
+                        word = str(word).replace('.', '').replace(',', '').replace("'", '').replace(':', '').replace('?', '').lower()
+                        if word not in sw:
+                            yield autor + ' :: ' + word, 1
 
 # REDUCE
 # Name node especifica workers para executar a função REDUCE após MAP ser realizado
@@ -44,25 +47,47 @@ def mapfn(k, v):
 def reducefn(k, v):
     return sum(v)
 
+# Server
 s = mincemeat.Server()
 s.datasource = source
 s.mapfn = mapfn
 s.reducefn = reducefn
 
+# Resultado Reduce
 results = s.run_server(password="changeme")
 
 w = csv.writer(open('result2.csv', 'w'))
 
 L = list()
+P = {}
+P2 = {}
+G = {}
+F = True
+
+# 2 autores e seus termos
 for k, v in results.items():
 
     a = k.split(' :: ')[0]
-    print(a)
 
-    if a not in L:
-        w.writerow([k.split(' :: ')[0]])
-        w.writerow([k.split(' :: ')[1], str(v)])
-        L.append(a)
-    else:
-        w.writerow([k.split(' :: ')[1], str(v)])
+    if a == 'Grzegorz Rozenberg':
+        P[k.split(' :: ')[1]] = v
 
+    if a == 'Philip S. Yu':
+        P2[k.split(' :: ')[1]] = v
+
+    G['Grzegorz Rozenberg'] = P
+    G['Philip S. Yu'] = P2
+
+# 2 maiores pontuadores de cada
+M = list()
+for item in G.values():
+    for dict_item in sorted(item, key=item.get, reverse=True):
+        M.append(item[dict_item])
+
+    maximo = max(M)
+
+    for dict_item in sorted(item, key=item.get, reverse=True):
+        if item[dict_item] == maximo:
+            print(dict_item)
+
+    M = list()
